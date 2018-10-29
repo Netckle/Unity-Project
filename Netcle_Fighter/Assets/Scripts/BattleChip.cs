7b7a2659ae30714c13614c880a2ae5a0a1619fc1
Bluenet <33153152+Bleunet@users.noreply.github.com>
2018-10-30 00:14:29 +0900

    20181030

---
+++ Netcle_Fighter/Assets/Scripts/BattleChip.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 배틀칩의 속성 열거형
+public enum STATE
+{
+    NORMAL,
+    FIRE,
+    ICE
+}
+
+public class BattleChip : MonoBehaviour
+{
+
+    public STATE chipState = STATE.NORMAL;
+
+    public float damage;
+    public float coolTime;
+
+    public int maxIndex; // 3
+
+    [TextArea(3, 10)]
+    public string explanation;
+
+    public Vector3[] chipPoints;
+    public GameObject colliderSpace;
+    private GameObject[] chipColliders;
+
+    void Start()
+    {
+
+    }
+
+    // 배틀칩의 변수들을 초기화하는 함수
+    public void Instantiate()
+    {
+        chipColliders = new GameObject[maxIndex];
+
+        switch (chipState)
+        {
+            case STATE.NORMAL:
+                {
+
+                }
+                break;
+            case STATE.FIRE:
+                {
+
+                }
+                break;
+            case STATE.ICE:
+                {
+
+                }
+                break;
+        }
+    }
+
+    // 배틀칩에 저장된 경로의 시작점을 플레이어의 위치를 기준으로 재설정하는 함수
+    public void AttachToPlayerPos(GameObject player)
+    {
+        for (int i = 0; i < maxIndex; i++) 
+        {
+            chipPoints[i].Set(
+                player.transform.position.x + chipPoints[i].x,
+                player.transform.position.y + chipPoints[i].y,
+                player.transform.position.z + chipPoints[i].z
+                );
+        }
+    }
+
+    // 배틀칩의 기록된 범위와 경로에 콜라이더 생성 및 제거하는 함수
+    public IEnumerator executeAct()
+    {
+        for (int i = 0; i < maxIndex; i++)
+        {
+            chipColliders[i] = Instantiate(colliderSpace, chipPoints[i], Quaternion.identity) as GameObject;
+
+            yield return new WaitForSeconds(coolTime);
+        }
+
+        yield return new WaitForSeconds(1.0F);
+
+        for (int i = 0; i < maxIndex; i++)
+        {
+            Destroy(chipColliders[i]);
+
+            yield return new WaitForSeconds(coolTime);
+        }
+    }
+}
---
+++ Netcle_Fighter/Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public BattleChip normalAttackChip;
+    //public BattleChip[] skillAttackChip;
+
+    public float hp = 100;
+
+    private Vector3 movement;
+
+    void Start()
+    {
+        normalAttackChip.Instantiate();        
+    }
+
+    void Update()
+    {
+        Move();
+        NormalAttack();
+    }
+
+    // 이동 함수
+    void Move()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            movement = new Vector3(0, 0, 1);
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+            movement = new Vector3(0, 0, -1);
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+            movement = new Vector3(1, 0, 0);
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+            movement = new Vector3(-1, 0, 0);
+
+        transform.position += movement;
+
+        movement = Vector3.zero;
+    }
+
+    // 일반 공격 함수
+    void NormalAttack()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            normalAttackChip.AttachToPlayerPos(this.gameObject);
+            StartCoroutine(normalAttackChip.executeAct());
+        }
+    }
+}
