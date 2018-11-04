95ce405afcfb4301992fad9e9395c2a2f031f867
Bluenet <33153152+Bleunet@users.noreply.github.com>
2018-11-04 23:22:39 +0900

    20181104

---
+++ Project_D/Assets/BoxSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxSpawner : MonoBehaviour {
+
+    public GameObject box;
+    public bool canSpawn = true;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (canSpawn)
+            StartCoroutine(SpawnBox());
+    }
+
+    IEnumerator SpawnBox()
+    {
+        canSpawn = false;
+
+        GameObject BoxObj = Instantiate(box, transform.position, Quaternion.identity) as GameObject;
+
+        yield return new WaitForSeconds(0.5F);
+
+        canSpawn = true;
+    }
+}
---
+++ Project_D/Assets/Player.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // ----- [Move Variable]
+    private float moveInput;
+    public float moveSpeed;
+
+    // ----- [Jump Variable]
+    public float jumpVelocity;
+    public float fallMultiplier = 2.5F;
+    public float lowJumpMultiplier = 2.0F;
+
+    private Rigidbody2D rigid;
+
+    // ----- [Ground Check Variable]
+    private bool isGrounded = false;
+    public Transform feetPos;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    // ----- [Override Function]
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+        Move();
+        Jump();
+    }
+
+    // ----- [Action Function]
+    void Move()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+
+        rigid.velocity = new Vector2(moveInput * moveSpeed, rigid.velocity.y);
+
+        if (moveInput > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else if (moveInput < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+    }
+    void Jump()
+    {
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+
+        if (isGrounded == true && Input.GetButtonDown("Jump"))
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpVelocity;
+        }
+
+        if (rigid.velocity.y < 0)
+        {
+            rigid.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rigid.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rigid.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    // ----- [Debug Function]
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(feetPos.position, checkRadius);
+    }
+}
