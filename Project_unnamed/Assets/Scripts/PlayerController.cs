a8a3c5dd4328e78e7255f756b0e2b9e7edae3699
bluenet <junyoung246@gmail.com>
2019-01-04 09:16:14 +0900

    first commit

---
+++ Project_unnamed/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Public 변수
+    public float moveSpeed = 1.0f;
+    //Private 변수
+    Rigidbody2D rigid;
+    Vector3 movement;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+        Move();
+    }
+    void Move()
+    {
+        Vector3 moveVelocity = Vector3.zero;
+
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            moveVelocity = Vector3.left;
+        }
+
+        else if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            moveVelocity = Vector3.right;
+        }
+
+        transform.position += moveVelocity * moveSpeed * Time.deltaTime;
+    }
+}
