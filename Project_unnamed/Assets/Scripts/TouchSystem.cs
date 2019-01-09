ade5da9513ad1dd45879739759126dfc537c4ee4
bluenet <junyoung246@gmail.com>
2019-01-09 21:38:45 +0900

    first commit

---
+++ Project_unnamed/Assets/Scripts/TouchSystem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchSystem : MonoBehaviour
+{
+    private Vector3 mousePos;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("마우스 좌표 : " + Input.mousePosition);
+            mousePos = Input.mousePosition;
+        }
+    }
+}
