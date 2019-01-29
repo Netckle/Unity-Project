1c64584ec59688428e0afd40dd6887bdfafaae8c
bluenet <junyoung246@gmail.com>
2019-01-29 21:13:29 +0900

    first commit

--- RPG MAKER GAME/Assets/Script/MovingObject.cs
+++ RPG MAKER GAME/Assets/Script/MovingObject.cs
@@ -22,48 +22,65 @@ public class MovingObject : MonoBehaviour
     private bool canMove = true;
     private bool applyRunFlag = false;
 
-    IEnumerator MoveCoroutine()
-    {
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            applyRunSpeed = runSpeed;
-            applyRunFlag = true;
-        }
-        else
-        {
-            applyRunSpeed = 0;
-            applyRunFlag = false;
-        }
-
-        vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
-
-        while(currentWalkCount < walkCount)
-        {
-            if(vector.x != 0)
-            {
-                transform.Translate(vector.x * (speed + applyRunSpeed), 0, 0);
-            }
-            else if (vector.y != 0)
-            {
-                transform.Translate(0, vector.y * (speed + applyRunSpeed), 0);
-            }
-
-            if (applyRunFlag)
-            {
-                currentWalkCount++;
-            }
-
-            currentWalkCount++;
-            yield return new WaitForSeconds(0.01f);
-        }
-        currentWalkCount = 0; 
-        
-        canMove = true;
-    }
+    private Animator animator;
 
     void Start()
     {
+        animator = GetComponent<Animator>();
+    }
 
+    IEnumerator MoveCoroutine()
+    {
+        while(Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0)
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                applyRunSpeed = runSpeed;
+                applyRunFlag = true;
+            }
+            else
+            {
+                applyRunSpeed = 0;
+                applyRunFlag = false;
+            }
+
+            vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
+
+            if (vector.x != 0)
+                vector.y = 0;
+
+            if (vector.y != 0)
+                vector.z = 0;
+
+            animator.SetFloat("DirX", vector.x);
+            animator.SetFloat("DirY", vector.y);
+            animator.SetBool("Walking", true);
+
+            while(currentWalkCount < walkCount)
+            {
+                if(vector.x != 0)
+                {
+                    transform.Translate(vector.x * (speed + applyRunSpeed), 0, 0);
+                }
+                else if (vector.y != 0)
+                {
+                    transform.Translate(0, vector.y * (speed + applyRunSpeed), 0);
+                }
+
+                if (applyRunFlag)
+                {
+                    currentWalkCount++;
+                }
+
+                currentWalkCount++;
+                yield return new WaitForSeconds(0.01f);
+            }
+
+            currentWalkCount = 0;       
+        }   
+
+        animator.SetBool("Walking", false);
+        canMove = true;  
     }
 
     void Update()
