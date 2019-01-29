18ccca3ce6c1049af3728cf4e7dd61f42a4ab65f
bluenet <junyoung246@gmail.com>
2019-01-29 12:07:15 +0900

    first commit

---
+++ RPG MAKER GAME/Assets/Script/MovingObject.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour
+{
+    public float speed;
+
+    private Vector3 vector;
+
+    public float runSpeed;
+    private float applyRunSpeed;
+
+    public int walkCount;
+    private int currentWalkCount;
+
+    // speed = 2.4, walkCount = 20
+    // 2.4 * 20 = 48 pixel
+    // while 
+    // currentWalkCount += 1, 20   
+
+    private bool canMove = true;
+    private bool applyRunFlag = false;
+
+    IEnumerator MoveCoroutine()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            applyRunSpeed = runSpeed;
+            applyRunFlag = true;
+        }
+        else
+        {
+            applyRunSpeed = 0;
+            applyRunFlag = false;
+        }
+
+        vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
+
+        while(currentWalkCount < walkCount)
+        {
+            if(vector.x != 0)
+            {
+                transform.Translate(vector.x * (speed + applyRunSpeed), 0, 0);
+            }
+            else if (vector.y != 0)
+            {
+                transform.Translate(0, vector.y * (speed + applyRunSpeed), 0);
+            }
+
+            if (applyRunFlag)
+            {
+                currentWalkCount++;
+            }
+
+            currentWalkCount++;
+            yield return new WaitForSeconds(0.01f);
+        }
+        currentWalkCount = 0; 
+        
+        canMove = true;
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {        
+        if (canMove)
+        {
+            if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+            {
+                canMove = false;
+                StartCoroutine(MoveCoroutine());        
+            }
+        }        
+    }
+}
