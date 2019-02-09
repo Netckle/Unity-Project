741d3d1969c84ef9b09bade9b5b7ff85eb3cfc7b
Netckle <33153152+Netckle@users.noreply.github.com>
2019-02-09 13:12:29 +0900

    first commit

---
+++ RPG Style Game/Assets/Scripts/TestScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    BGMManager BGM;
+
+    public int playMusicTrack;
+
+    void Start()
+    {
+        BGM = FindObjectOfType<BGMManager>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        BGM.Play(playMusicTrack);
+        this.gameObject.SetActive(false);
+    }
+}
