17979110ddad13f6119b802cdea0b8930c348c01
Netckle <33153152+Netckle@users.noreply.github.com>
2019-02-18 22:21:25 +0900

    first commit
    InputField 이용법

---
+++ RPG Style Game/Assets/Scripts/InputField.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputField : MonoBehaviour
+{
+    private PlayerManager thePlayer;
+    public Text text;
+
+    void Start()
+    {
+        thePlayer = FindObjectOfType<PlayerManager>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            thePlayer.characterName = text.text;
+            Destroy(this.gameObject);
+        }
+    }
+}
