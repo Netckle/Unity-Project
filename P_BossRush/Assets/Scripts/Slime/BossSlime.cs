657c3684e4d5749bdf168e95cf8e6a740e840a53
Netckle <33153152+Netckle@users.noreply.github.com>
2019-06-03 18:53:24 +0900

    first commit

---
+++ P_BossRush/Assets/Scripts/Slime/BossSlime.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSlime : MonoBehaviour
+{
+    [Header("State")]
+    public int HP;
+    public int maxMiniSlime;
+    public float scale;
+
+    [Header("Movement")]
+    public int movePower;
+
+    [Header("Flag")]
+    public bool canAttack;
+
+    private Vector3 move;
+    private int movementFlag = 0;    
+    private Animator anim;
+    private SpawnMiniSlime spawn;
+
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+        spawn = GetComponent<SpawnMiniSlime>();
+
+        StartCoroutine("ChangeMovement");
+    }
+
+    IEnumerator ChangeMovement()
+    {
+        movementFlag = Random.Range(0, 3);
+
+        if (movementFlag == 0)
+            anim.SetBool("isMoving", false);
+        else
+            anim.SetBool("isMoving", true);
+
+        yield return new WaitForSeconds(3.0f);
+
+        StartCoroutine("ChangeMovement");
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            spawn.SpawnMiniMonster(3);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 moveVelocity = Vector3.zero;
+
+        if (movementFlag == 1)
+        {
+            moveVelocity = Vector3.left;
+            transform.localScale = new Vector3(1, 1, 1) * scale;
+        }
+        else if (movementFlag == 2)
+        {
+            moveVelocity = Vector3.right;
+            transform.localScale = new Vector3 (-1, 1, 1) * scale;
+        }
+        transform.position += moveVelocity * movePower * Time.deltaTime;
+    }
+}
---
+++ P_BossRush/Assets/Scripts/Slime/MiniSlime.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniSlime : MonoBehaviour
+{
+    [Header("State")]
+    public int HP;
+
+    [Header("Movement")]
+    public int movePower;
+
+    private Vector3 move;
+    private int movementFlag = 0;    
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+
+        StartCoroutine("ChangeMovement");
+    }
+
+    IEnumerator ChangeMovement()
+    {
+        movementFlag = Random.Range(0, 3);
+
+        if (movementFlag == 0)
+            anim.SetBool("isMoving", false);
+        else
+            anim.SetBool("isMoving", true);
+
+        yield return new WaitForSeconds(3.0f);
+
+        StartCoroutine("ChangeMovement");
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 moveVelocity = Vector3.zero;
+
+        if (movementFlag == 1)
+        {
+            moveVelocity = Vector3.left;
+            transform.localScale = new Vector3 (1, 1, 1);
+        }
+        else if (movementFlag == 2)
+        {
+            moveVelocity = Vector3.right;
+            transform.localScale = new Vector3 (-1, 1, 1);
+        }
+        transform.position += moveVelocity * movePower * Time.deltaTime;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // 임시로 해놓음.
+        if (other.gameObject.tag == "Player")
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
