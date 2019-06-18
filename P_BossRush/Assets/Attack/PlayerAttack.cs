e36b7d85f4169356077a3cdbb8567a5d53883f57
Netckle <33153152+Netckle@users.noreply.github.com>
2019-06-18 23:38:08 +0900

    first commit

---
+++ P_BossRush/Assets/Attack/PlayerAttack.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
+
+    public Transform attackPos;
+    public LayerMask whatIsEnemies;
+    public Animator camAnim;
+    public Animator playerAnim;
+
+    // public float attackRangeX, attackRangeY
+    public float attackRange;
+    public int damage;
+
+    void Update()
+    {
+        if (timeBtwAttack <= 0)
+        {
+            // then you can attack
+            if (Input.GetKey(KeyCode.J))
+            {
+                camAnim.SetTrigger("shake");
+                playerAnim.SetTrigger("attack");
+
+                timeBtwAttack = startTimeBtwAttack;
+                // Collider2D[] ** = Phyiscs2D.OverlapBoxAll(attackPos.position, new Vector2(attackRangeX, attackRangeY), 0, whatIsEnemies);
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+                for (int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
+                }
+            }            
+        }
+        else
+        {
+            timeBtwAttack -= Time.deltaTime;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        // Gizmos.DrawWireCube(attackPos.position, new Vector3(attackRangeX, attackRangeY, 1));
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
+}
